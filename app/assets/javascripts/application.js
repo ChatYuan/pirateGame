9a51be1d5d50d1b2ab594931109b05cc3811a134
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-01-02 13:07:10 +0000

    Revert "macvlan : fix checksums error when we are in bridge mode"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,4 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
+// It's not advisable to ad
