3c0c5b38e8b74b7d8617a7f508cffca6dd48119f
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-01-03 09:13:59 +0000

    Correciones

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,4 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if y
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom
