535d79e593c2cc11fa40574ced4fa4c8956ebde4
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-01-04 08:38:06 +0000

    Remove log line

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,5 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file
