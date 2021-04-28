6752753a13687604b06e0bb5a568fee34eeccc51
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-04-28 10:05:35 +0000

    net: fix sparse warning in sk_dst_set()

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,4 +19,4 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's stat
+  # Disable Rails's static asset server (Apache or ngi
