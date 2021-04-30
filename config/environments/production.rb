b5978176676b44ac580ceecf603980fd3821d885
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-04-30 19:03:29 +0000

    6.0.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets
+  config.serve_static_assets = false
+
+  # Compre
