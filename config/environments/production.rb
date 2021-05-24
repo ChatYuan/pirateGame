8f434b885f94240200788ee078d5d6ffb03a71a7
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-05-24 12:04:03 +0000

    Updated by script

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force
+  # config.force_ssl = true
+
+  # Set to :debug
