e3c04866eaa3b5b8fb1ea83711b9256d8a7820cc
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-05-28 14:23:47 +0000

    src/JsRender/NodeToJs.vala

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,4 +41,5 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Set to :debug to see everything in t
+  # Set to :debug to see everything in the log.
+  config.log_level = :i
