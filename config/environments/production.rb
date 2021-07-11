faab0a31f54aaea98aa7add1697f5211054dc891
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-07-11 11:43:09 +0000

    Add missing dependencies.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,6 @@ Rails.application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::L
+  config.log_formatter = ::Logger::Formatter.new
+
+  # Do not dump sch
