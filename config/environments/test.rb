0fa86a0f6431ac8d1516b766abef252adb51627d
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-07-30 19:03:42 +0000

    fix a bug

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,4 +2,5 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwis
+  # test suite. You never need to work with it otherwise. Remember that
+  # your t
