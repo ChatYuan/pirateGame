4508cbf6362af9896c59ccbde76962551056e510
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-08-18 12:13:38 +0000

    Added new problem

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,4 +17,5 @@ Rails.application.configure do
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = tr
+  config.consider_all_requests_local       = true
+  config.action_controlle
