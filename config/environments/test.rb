184788bc79850301333164745f170dd96cb25396
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-08-26 06:53:32 +0000

    Simplify readme

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,4 +23,4 @@ Rails.application.configure do
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery 
+  # Disable request forgery protection in test e
