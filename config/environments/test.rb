f123ef3719f931538736c52c859846b4ce52cf3e
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-08-29 07:09:55 +0000

    Updated _config.yml

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,4 +24,6 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protec
+  config.action_controller.allow_forgery_protection = false
+
+  # Tell Action Mai
