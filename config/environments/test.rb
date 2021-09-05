715402238ac06c214efe46ede638cd997221cff7
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-09-05 21:51:12 +0000

    1.1.23

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,4 +28,5 @@ Rails.application.configure do
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliv
+  # ActionMailer::Base.deliveries array.
+  config.action_m
