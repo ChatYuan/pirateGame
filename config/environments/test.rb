5797b362c9ffb046912d6c2d3ed3e391fe2a5174
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-09-08 06:40:10 +0000

    Update card.php

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,4 +29,4 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_m
+  config.action_mailer.delivery_m
