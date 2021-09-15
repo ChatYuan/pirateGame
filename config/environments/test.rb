db311cd2f6bc7b853c9fe419de08853af5bcad4d
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-09-15 23:09:11 +0000

    code tidy up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_support.dep
+  config.active_support.deprecation = :stderr
+
+  # Raises error
