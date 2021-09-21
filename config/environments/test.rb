f349217e1a3c25e906fb0ca1edea796844ea3a00
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-09-21 18:05:55 +0000

    staging: zsmalloc: Ensure handle is never 0 on success

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,4 +35,5 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  # config.action_view.raise_on_mis
+  # config.action_view.raise_on_missing_translations = true
+end
