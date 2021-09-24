b19a2248b73d422b34be84a5be41babfbb554271
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-09-24 21:41:30 +0000

    pedantic--

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,5 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Precompile additional 
+# Precompile additional assets.
+# application.j
