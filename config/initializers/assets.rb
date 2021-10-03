85d6ec8ca279ce781af16e4324b6a87cf01e423c
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-10-03 17:52:32 +0000

    bridge: set priority of STP packets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,5 @@
 Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already a
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.confi
