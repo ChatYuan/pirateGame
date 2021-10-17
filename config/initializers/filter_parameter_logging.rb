68266f6f20893798ece02c9099c6d95b7770bf86
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-10-17 12:51:31 +0000

    dm btree remove: fix bug in redistribute3

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.appli
+Rails.application.config.filter_pa
