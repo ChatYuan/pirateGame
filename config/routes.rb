d4d95d5989433886dba64ce935a1c404a265b171
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-12-05 11:46:12 +0000

    rename library

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,4 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how a
+  # See how all your routes lay out wit
