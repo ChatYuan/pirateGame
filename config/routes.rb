3e752ee61f199fe423dcbcb7b0068d024771d9b5
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-12-10 08:59:11 +0000

    PEP8 compliance.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out wit
+  # See how all your routes lay out with "rake routes".
+
+  
