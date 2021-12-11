7a1e182c82ed0d08770882e8d8886ebe13c45e4b
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-12-11 22:50:02 +0000

    unit test update

--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+   root 'home#index'
+
+  # Exa
