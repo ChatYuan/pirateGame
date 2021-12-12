97223a2ad764010ea608bea7875954206725fd3b
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-12-12 14:31:50 +0000

    Wrong variable

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'home#index'
 
-  # Example of regular route
+  # Example of regular route:
+  #   get 'products/:id' => '
