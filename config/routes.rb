2ed9eb577d87af6fb495ad886afc26899689f648
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2021-12-14 11:46:48 +0000

    drm/i915: do not ignore eDP bpc settings from vbt

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id:
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   ge
