4b56c2153602841c6891067d5277f9bbe719afd8
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2022-01-15 09:32:07 +0000

    Fix potential memory leak

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,5 @@ Rails.application.routes.draw do
 
   # Example resource route with sub-resources:
   #   resources :products do
-  #     resources 
+  #     resources :comments, :sales
+  #     resou
