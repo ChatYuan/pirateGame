55798dcd88572294a82dac2f1ee927f978446094
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2022-01-24 21:58:19 +0000

    Added Friulian translation

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,5 @@ Rails.application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #
+  #     resources :products
+  #   en
