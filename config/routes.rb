3471e0c3aa79c29ec225ff34c829291907537100
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2022-01-24 21:41:19 +0000

    Fixed CSS

--- config/routes.rb
+++ config/routes.rb
@@ -52,4 +52,5 @@ Rails.application.routes.draw do
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/prod
+  #     # (app/controllers/admin/products_controller.rb)
+  #
