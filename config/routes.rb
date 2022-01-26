0b5560a9c77077f23c3bb104a3466d1dc95a23ff
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2022-01-26 11:14:51 +0000

    Fixed php-cs-fixer and php-formatter version

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,5 @@ Rails.application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   en
+  #   end
+end
