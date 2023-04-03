eae275da34b316b161c57fd29ee5b709a095c872
ChatYuan <124684299+ChatYuan@users.noreply.github.com>
2023-04-03 11:46:54 +0000

    Update doc 7.1

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe HomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:succes
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
